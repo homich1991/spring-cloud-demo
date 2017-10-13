83725266ad02ad9ec393edaf8c137dd8ba596f70
Roman_Meerson <Roman_Meerson@epam.com>
2017-10-13 18:37:45 +0400

    eureka works

--- receiver/src/main/java/com/epam/springclouddemo/ReceiverApplication.java
+++ receiver/src/main/java/com/epam/springclouddemo/ReceiverApplication.java
@@ -2,12 +2,14 @@ package com.epam.springclouddemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@EnableEurekaClient
 public class ReceiverApplication {
 
     public static void main(String[] args) {
@@ -17,7 +19,7 @@ public class ReceiverApplication {
     @GetMapping("/multiply/{counter}")
     public Integer multiplyCounter(@PathVariable int counter) {
         System.out.println("I receive " + counter);
-        System.out.println("I send " + counter*2);
+        System.out.println("I send " + counter * 2);
         return counter * 2;
     }
 }
